9e4b7eabb091bd3de0b488f9e02230f61dab183c
ndkienhd2004 <kienhd2004@gmail.com>
2024-05-19 19:25:17 +0700

    Update UI

--- src/components/homeItems/WingletsCard.jsx
+++ src/components/homeItems/WingletsCard.jsx
@@ -1,14 +1,18 @@
-import React, { useState, useMemo, useRef } from "react";
+import React, { useState, useMemo, useRef, useEffect } from "react";
 import TinderCard from "react-tinder-card";
 import Profile from "../profileItems/listProfile";
 import DoneIcon from "@mui/icons-material/Done";
 import ClearIcon from "@mui/icons-material/Clear";
 import UndoIcon from "@mui/icons-material/Undo";
-import { useEffect } from "react";
+import PopupCard from "./PopupCard";
+import { Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 function WingletsCard({ matchedProfile, setMatchedProfile }) {
+  const navigate = useNavigate();
   const [currentIndex, setCurrentIndex] = useState(Profile.length - 1);
   const currentIndexRef = useRef(currentIndex);
-  const [currentView, setCurrentView] = useState(0);
+  const [checkProfile, setCheckProfile] = useState(null);
 
   const childRefs = useMemo(
     () =>
@@ -58,6 +62,7 @@ function WingletsCard({ matchedProfile, setMatchedProfile }) {
     updateCurrentIndex(newIndex);
     await childRefs[newIndex].current.restoreCard();
   };
+
   const containerStyles = {
     width: "1174px",
     height: "787px",
@@ -69,6 +74,7 @@ function WingletsCard({ matchedProfile, setMatchedProfile }) {
     position: "relative",
     overflow: "hidden",
   };
+
   const clearButtonStyles = {
     position: "absolute",
     bottom: "20px",
@@ -81,6 +87,7 @@ function WingletsCard({ matchedProfile, setMatchedProfile }) {
     width: "100px",
     height: "100px",
   };
+
   const doneButtonStyles = {
     position: "absolute",
     bottom: "20px",
@@ -93,6 +100,7 @@ function WingletsCard({ matchedProfile, setMatchedProfile }) {
     width: "100px",
     height: "100px",
   };
+
   const goBackButtonStyle = {
     position: "absolute",
     bottom: "20px",
@@ -106,43 +114,14 @@ function WingletsCard({ matchedProfile, setMatchedProfile }) {
     height: "100px",
   };
 
-  const [moving, setMoving] = useState(false);
-
-  const moveDown = () => {
-    if (!moving && currentView <= 0) {
-      setMoving(true);
-      setCurrentView((prevView) => prevView + 1);
-      setTimeout(() => {
-        setMoving(false);
-      }, 500);
-    }
-  };
-
-  const moveUp = () => {
-    if (!moving && currentView >= 0) {
-      setMoving(true);
-      setCurrentView((prevView) => prevView - 1);
-      setTimeout(() => {
-        setMoving(false);
-      }, 500);
-    }
-  };
   useEffect(() => {
     const handleKeyDown = (event) => {
       switch (event.key) {
-        case "ArrowUp":
-          moveUp();
-          break;
-        case "ArrowDown":
-          moveDown();
-          break;
         case "ArrowLeft":
           swipe("left");
-          setCurrentView(0);
           break;
         case "ArrowRight":
           swipe("right");
-          setCurrentView(0);
           break;
         default:
           break;
@@ -158,147 +137,62 @@ function WingletsCard({ matchedProfile, setMatchedProfile }) {
 
   return (
     <div>
-      <div style={containerStyles}>
-        {Profile.map((character, index) => (
-          <TinderCard
-            ref={childRefs[index]}
-            className="swipe"
-            key={character.name}
-            onSwipe={(dir) => swiped(dir, character.name, index)}
-            onCardLeftScreen={() => outOfFrame(character.name, index)}
-            preventSwipe={["up", "down"]}
-            style={{
-              position: "absolute",
-              top: 0,
-              transform: `translateY(${index * -100}%)`,
-              zIndex: Profile.length - index,
-              width: "100%",
-            }}
-          >
-            <div
-              className="card"
-              style={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                transform: `translateY(${index * 0}px)`,
-                zIndex: index === currentIndex ? 1 : 0,
-              }}
-            >
-              {currentView === 0 && (
+      {checkProfile ? (
+        <div>
+          <div style={containerStyles}>
+            {Profile.map((character, index) => (
+              <TinderCard
+                ref={childRefs[index]}
+                className="swipe"
+                key={character.name}
+                onSwipe={(dir) => swiped(dir, character.name, index)}
+                onCardLeftScreen={() => outOfFrame(character.name, index)}
+                preventSwipe={["up", "down"]}
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  transform: `translateY(${index * -100}%)`,
+                  zIndex: Profile.length - index,
+                  width: "100%",
+                }}
+              >
                 <div
-                  className="first-div"
+                  className="card"
                   style={{
-                    width: "1174px",
-                    height: "787px",
-                    backgroundColor: "#E27B83",
-                    borderRadius: "2rem",
-                    overflow: "hidden",
-                    display: "flex",
-                    flexDirection: "row",
-                    transition: "opacity 0.5s ease-in-out",
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    transform: `translateY(${index * 0}px)`,
+                    zIndex: index === currentIndex ? 1 : 0,
                   }}
                 >
-                  <div
-                    style={{
-                      flex: "0 0 50%",
-                      overflow: "hidden",
-                      borderRadius: "2rem 0 0 2rem",
-                      background: `url(${character.avatar})`,
-                      backgroundSize: "cover",
-                      backgroundPosition: "center",
-                      position: "relative",
-                    }}
-                  >
-                    <div
-                      style={{
-                        position: "absolute",
-                        bottom: "20px",
-                        left: "20px",
-                        color: "white",
-                        fontSize: "60px",
-                      }}
-                    >
-                      {character.name}, {character.age}
-                    </div>
-                  </div>
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      flex: "0 0 50%",
-                      wordWrap: "break-word",
-                      position: "relative",
-                      flexDirection: "column",
-                      textAlign: "center",
-                    }}
-                  >
-                    <h1>About {character.name}</h1>
-                    <span>{character.bio}</span>
-                  </div>
+                  <PopupCard character={character} />
                 </div>
-              )}
-              {currentView === 1 && (
-                <div
-                  className="second-div"
-                  style={{
-                    width: "1174px",
-                    height: "787px",
-                    backgroundColor: "#E27B83",
-                    borderRadius: "2rem",
-                    overflow: "hidden",
-                    display: "flex",
-                    flexDirection: "row",
-                  }}
-                >
-                  <div
-                    style={{
-                      flex: "0 0 50%",
-                      overflow: "hidden",
-                      borderRadius: "2rem 0 0 2rem",
-                      background: `url(${character.images[0]})`,
-                      backgroundSize: "cover",
-                      backgroundPosition: "center",
-                      position: "relative",
-                    }}
-                  ></div>
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      flex: "0 0 50%",
-                      wordWrap: "break-word",
-                      position: "relative",
-                      flexDirection: "column",
-                      textAlign: "center",
-                    }}
-                  >
-                    <h1>Hobby: {character.hobby}</h1>
-                    <span>
-                      {character.horoscope} ,{character.job}
-                    </span>
-                  </div>
-                </div>
-              )}
-            </div>
-          </TinderCard>
-        ))}
-      </div>
-      <div className="button">
-        <button style={clearButtonStyles} onClick={() => swipe("left")}>
-          <ClearIcon sx={{ fontSize: "60px" }} />
-        </button>
-        <button style={goBackButtonStyle} onClick={() => goBack()}>
-          <UndoIcon sx={{ fontSize: "60px" }} />
-        </button>
-        <button style={doneButtonStyles} onClick={() => swipe("right")}>
-          <DoneIcon sx={{ fontSize: "60px" }} />
-        </button>
-      </div>
+              </TinderCard>
+            ))}
+          </div>
+          <div className="button">
+            <button style={clearButtonStyles} onClick={() => swipe("left")}>
+              <ClearIcon sx={{ fontSize: "60px" }} />
+            </button>
+            <button style={goBackButtonStyle} onClick={() => goBack()}>
+              <UndoIcon sx={{ fontSize: "60px" }} />
+            </button>
+            <button style={doneButtonStyles} onClick={() => swipe("right")}>
+              <DoneIcon sx={{ fontSize: "60px" }} />
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <Typography>Please enter your information</Typography>
+          <button onClick={() => navigate("/Q&A")}>
+            go to Create Profile page
+          </button>
+        </div>
+      )}
     </div>
   );
 }
