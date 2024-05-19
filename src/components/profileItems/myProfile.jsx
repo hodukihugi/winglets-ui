1cac969bcc131563fae884bf7c0e28e354d26a15
ndkienhd2004 <kienhd2004@gmail.com>
2024-05-19 21:47:25 +0700

    update UI

--- src/components/profileItems/myProfile.jsx
+++ src/components/profileItems/myProfile.jsx
@@ -1,18 +1,15 @@
-import { useState } from "react";
-
-const MyProfile = () => {
-  const [images, setImages] = useState([]);
-
-  const avatar =
-    "https://static.vecteezy.com/system/resources/previews/002/275/847/original/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg";
-
-  return {
-    avatar: avatar,
-    images: images,
-    name: "abcxyz",
-    age: "20",
-    location: "London",
-  };
+const MyProfile = {
+  avatar:
+    "https://static.vecteezy.com/system/resources/previews/002/275/847/original/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg",
+  images: [
+    "https://www.shutterstock.com/image-photo/profile-picture-smiling-successful-young-260nw-2040223583.jpg",
+    "https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg",
+    "https://t3.ftcdn.net/jpg/04/60/91/88/360_F_460918802_XVCymFr7MoziFpnInbTDvrlblYhvAOi2.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIi-_K-lJoInpIdhSuyuU-Jvx6_EW0XDjXem3JUdFaREoGQWcG5kTlytmwHfdQhJHeX1M&usqp=CAU",
+  ],
+  name: "abcxyz",
+  age: "20",
+  location: "London",
 };
 
 export default MyProfile;
